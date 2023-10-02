61eb9aa15523ba453928051f9f0ab7e15a775ab3
Isaac Oseas <oseasisaac91@gmail.com>
2023-10-02 17:15:25 -0400

    Update my-meow.el

--- my-meow.el
+++ my-meow.el
@@ -2,36 +2,115 @@
 ;;   (interactive "c" )
 ;;   (execute-kbd-macro (kbd (concat "C-" (string char)))))
 
+
+(defun thing-at-point-goto-end-of-integer ()
+  "Go to end of integer at point."
+  (let ((inhibit-changing-match-data t))
+    ;; Skip over optional sign
+    (when (looking-at "[+-]")
+      (forward-char 1))
+    ;; Skip over digits
+    (skip-chars-forward "[[:digit:]]")
+    ;; Check for at least one digit
+    (unless (looking-back "[[:digit:]]")
+      (error "No integer here"))))
+(put 'integer 'beginning-op 'thing-at-point-goto-end-of-integer)
+
+(defun thing-at-point-goto-beginning-of-integer ()
+  "Go to end of integer at point."
+  (let ((inhibit-changing-match-data t))
+    ;; Skip backward over digits
+    (skip-chars-backward "[[:digit:]]")
+    ;; Check for digits and optional sign
+    (unless (looking-at "[+-]?[[:digit:]]")
+      (error "No integer here"))
+    ;; Skip backward over optional sign
+    (when (looking-back "[+-]")
+        (backward-char 1))))
+(put 'integer 'beginning-op 'thing-at-point-goto-beginning-of-integer)
+
+(defun thing-at-point-bounds-of-integer-at-point ()
+  "Get boundaries of integer at point."
+  (save-excursion
+    (let (beg end)
+      (thing-at-point-goto-beginning-of-integer)
+      (setq beg (point))
+      (thing-at-point-goto-end-of-integer)
+      (setq end (point))
+      (cons beg end))))
+(put 'integer 'bounds-of-thing-at-point 'thing-at-point-bounds-of-integer-at-point)
+
+(defun thing-at-point-integer-at-point ()
+  "Get integer at point."
+  (let ((bounds (bounds-of-thing-at-point 'integer)))
+    (string-to-number (buffer-substring (car bounds) (cdr bounds)))))
+(put 'integer 'thing-at-point 'thing-at-point-integer-at-point)
+
+(defun increment-integer-at-point (&optional inc)
+  "Increment integer at point by one.
+
+With numeric prefix arg INC, increment the integer by INC amount."
+  (interactive "p")
+  (let ((inc (or inc 1))
+        (n (thing-at-point 'integer))
+        (bounds (bounds-of-thing-at-point 'integer)))
+    (delete-region (car bounds) (cdr bounds))
+    (insert (int-to-string (+ n inc)))))
+
+(defun decrement-integer-at-point (&optional dec)
+  "Decrement integer at point by one.
+
+With numeric prefix arg DEC, decrement the integer by DEC amount."
+  (interactive "p")
+  (increment-integer-at-point (- (or dec 1))))
+
+
   (defun meow-setup ()
     ;;(setq meow-cheatsheet-layout meow-cheatsheet-layout-colemak)
     (meow-motion-overwrite-define-key
      ;; Use e to move up, n to move down.
      ;; Since special modes usually use n to move down, we only overwrite e here.
+     '("n" . meow-next)
      '("e" . meow-prev)
      '("<escape>" . ignore))
     (meow-leader-define-key
      '("?" . meow-cheatsheet)
      ;; To execute the originally e in MOTION state, use SPC e.
+     '("N" . "H-n")
      ;; '("e" . "H-e")
      '("E" . "H-e")
      ;; '("e" . ctrl-prefix)
 
      '("bb" .  switch-to-buffer)
-     '("B" .  consult-buffer)
+     ;; '("B" .  consult-buffer)
      '("bi" .  ibuffer)
-     '("bs" .  crux-sudo-edit)
-     '("bc" .  crux-cleanup-buffer-or-region)
+     ;; '("bs" .  crux-sudo-edit)
+     ;; '("bc" .  crux-cleanup-buffer-or-region)
      '("bk" .  kill-buffer)
      '("bt" .  kill-this-buffer)
      '("bm" .  switch-to-minibuffer)
 
-     '("g s" . magit-status)
+     ;; '("g s" . magit-status)
+     '("l" . "C-c l") ;; used in LSP mode
+     
+     '("n r" . narrow-to-region)
+
+     '("o a" . org-agenda)
+     ;; '("o s" . eshell)
+     '("o c" . org-cycle-agenda-files)
 
      '("p" . "C-x p") ;;project prefix map
 
      '("r" . "C-x r") ;; mostly functions involving the register
+
+     '("w v" . split-window-right)
+     '("w s" . split-window-below)
+     '("w r" . delete-other-windows)
+     
      '("SPC" .  execute-extended-command)
-     '("," . embark-act)
+     '(";" . "M-;")
+     ;; '("," . embark-act)
+     ;; '("," . embark-act)
 
      '("1" . meow-digit-argument)
      '("2" . meow-digit-argument)
@@ -113,15 +192,23 @@
      '("x r" . anzu-query-replace-at-cursor)
      '("x s" . isearch-forward)
      '("x m" . meow-mark-symbol)
-     '("x k" . kmacro-toggle-start-end)
+     '("x k" . "<f3>")
      '("x i" . meow-open-below)
      '("x o" . crux-smart-open-line)
      '("x c" . "<f4>")
+     '("x (" . puni-wrap-round)
+     '("x {" . puni-wrap-curly)
+     '("x [" . puni-wrap-square)
      '("x SPC" . pop-to-mark-command)
 
      '("y" . meow-yank)
      '("z" . meow-pop-selection)
      '("'" . repeat)
+     '("[" . backward-paragraph)
+     '("]" . forward-paragraph)
+     '("+" . increment-integer-at-point)
+     '("_" . decrement-integer-at-point)
+
      '("<escape>" . ignore)))
 
   (use-package meow
@@ -130,7 +217,6 @@
     :config
     (global-set-key (kbd "C-x r C-.") #'point-to-register)
 
-    (setq meow-cursor-type-insert 'box)
     (setq meow-keypad-start-keys '((?i . ?c) ;;breaks naming convention but frees c in leader
                                    ;; (?h . ?h) ;;removed inorder to rebind h to C-M-
                                    (?t . ?x))) ;; easier to access than x
@@ -144,26 +230,71 @@
     (setq meow-cursor-type-origami 'hollow)
     (setq meow-cursor-type-beacon '(box))
 
-    (add-hook 'meow-insert-mode-hook (lambda () (set-cursor-color "#87af87")))
-    (add-hook 'meow-normal-mode-hook (lambda () (set-cursor-color "white")))
+    (defun my/treesit-node-parent-at-point ()
+      "returns treesitter node at point or runs my/treesit-node-parent"
+  (let ((node (treesit-node-at (point)) ))
+    (if (= (treesit-node-start node) (point))
+        (my/treesit-node-parent node (point))
+      node)))
 
-    (meow-define-keys 'origami
-      '("<escape>" . meow-normal-mode)
-      '("n" . origami-next-fold)
-      '("e" . origami-previous-fold)
-      '("t" . origami-toggle-node)
-      '("o a" . origami-open-all-nodes)
-      '("c a" . origami-close-all-nodes)
-      '("SPC" . meow-keypad)
-      '("u" . meow-undo))
+    (defun my/treesit-node-parent (node node-beg)
+      "recursively looks up parent node
+the lookup skips over nodes with the same starting point as the child"
+  (let ((node-parent (treesit-node-parent node)))
+    (cond ((string= (treesit-node-type node-parent) "program") node-parent)
+          ((= (treesit-node-start node-parent) node-beg) (my/treesit-node-parent node-parent node-beg))
+          (t node-parent))))
+
+(defun meow-ts--get-node-at-point ()
+  (let ((node (my/treesit-node-parent-at-point)))
+    ;; TODO abort when node is not the right node
+    `(,(treesit-node-start node) . ,(treesit-node-end node))
+    ))
+(meow-thing-register 'ts-node #'meow-ts--get-node-at-point #'meow-ts--get-node-at-point)
+(add-to-list 'meow-char-thing-table '(?n . ts-node))
+
+(defun my/treesit-node-up (&optional arg)
+  (interactive )
+  (let ((node (my/treesit-node-parent-at-point)))
+    (goto-char (treesit-node-start node))))
+
+(defun my/treesit-node-down (&optional arg)
+  (interactive)
+(let ((node (treesit-node-at (point)) ))
+    (goto-char (treesit-node-start (treesit-node-child (treesit-node-parent node) 1)))))
+
+(defun my/treesit-forward-sibling (&optional arg)
+  (interactive)
+  (let ((node (my/treesit-node-parent-at-point) ))
+    (goto-char (treesit-node-start (treesit-node-next-sibling node)))))
+
+
+(defun my/treesit-backward-sibling (&optional arg)
+    (interactive)
+  (let ((node (treesit-node-at (point)) ))
+    (goto-char (treesit-node-start (treesit-node-prev-sibling node)))))
+
+    ;; (add-hook 'meow-insert-mode-hook (lambda () (set-cursor-color "#87af87")))
+    ;; (add-hook 'meow-normal-mode-hook (lambda () (set-cursor-color "white")))
+
+    ;; (meow-define-keys 'origami
+    ;;   '("<escape>" . meow-normal-mode)
+    ;;   '("n" . origami-next-fold)
+    ;;   '("e" . origami-previous-fold)
+    ;;   '("t" . origami-toggle-node)
+    ;;   '("o a" . origami-open-all-nodes)
+    ;;   '("c a" . origami-close-all-nodes)
+    ;;   '("SPC" . meow-keypad)
+    ;;   '("u" . meow-undo))
     (setq meow-use-clipboard t)
     (setq meow-replace-state-name-list
-          '((normal . "<N>")
-            (motion . "<M>")
-            (keypad . "<K>")
-            (insert . "<E>")
-            (beacon . "<B>")
-            (origami . "<O>")))
+          '((normal . "[N]")
+            (motion . "[M]")
+            (keypad . "[K]")
+            (insert . "[E]")
+            (beacon . "[B]")
+            ;; (origami . "<O>")
+            ))
   (meow-setup)
 
     ;;fixed meow-end-kmacro to allow it to be called via keypad
@@ -177,4 +308,18 @@
             (meow-motion-mode-p))
         (call-interactively #'kmacro-end-or-call-macro))
        )))
-
+;; (defun my/meow-ts-node-start (node)
+;;   (let ((node-start (treesit-node-start node))
+;;                 (region-start (region-beginning)))
+;;             (if (<= node-start region-start)
+;;                 (treesit-node-start (treesit-node-parent node))
+;;               node-start))
+;;    )
+;; (defun my/treesit-node-parent (node node-beg)
+;;   (let ((node-parent (treesit-node-parent node)))
+;;     (cond ((string= (treesit-node-type node-parent)) node-parent)
+;;           ((= (treesit-node-start node-parent) node-beg) (my/treesit-node-parent node-parent node-beg))
+;;           (t node-parent)
+;; )
+;;     )
+;;   )
