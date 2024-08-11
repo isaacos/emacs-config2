d60ec2db254bc4ef9d926ac7d957ea2e47bb912d
Isaac Oseas <oseasisaac91@gmail.com>
2024-08-11 13:10:31 -0400

    Update useful-hydras.el

--- my/useful-hydras.el
+++ my/useful-hydras.el
@@ -1,6 +1,6 @@
 (provide 'useful-hydras)
 
-(defvar xah-brackets '("“”" "()" "[]" "{}" "<>" "＜＞" "（）" "［］" "｛｝" "❛❜" "❝❞" "❨❩" "❪❫" "❴❵" "❬❭" "❮❯" "❰❱"))
+(defvar xah-brackets '("“”" "()" "[]" "{}" "<>" "＜＞" "（）" "［］" "｛｝" "❛❜" "❝❞" "❨❩" "❪❫" "❴❵" "❬❭" "❮❯" "❰❱" "\"\""))
 
               (defconst xah-left-brackets
                 (mapcar (lambda (x) (substring x 0 1)) xah-brackets)
@@ -37,76 +37,83 @@
                     (re-search-forward (regexp-opt xah-left-brackets) (looking-at (regexp-opt xah-right-brackets)) t)
                     (backward-char 1))))
 
-              (defun xah-forward-right-bracket ()
-                "Move cursor to the next occurrence of right bracket.
+(defun xah-forward-right-bracket ()
+  "Move cursor to the next occurrence of right bracket.
               The list of brackets to jump to is defined by `xah-right-brackets'.
               URL `http://xahlee.info/emacs/emacs/emacs_navigating_keys_for_brackets.html'
               Version 2015-10-01"
-                (interactive)
-                (re-search-forward (regexp-opt xah-right-brackets) nil t))
+  (interactive)
+  (re-search-forward (regexp-opt xah-right-brackets) nil t))
 
-              (defun avy-goto-open-brackets ()
-                (interactive)
-                (avy-jump (regexp-opt xah-left-brackets)))
+(defun avy-goto-xah-open ( &optional arg)
+  "Jump to the currently visible CHAR.
+The window scope is determined by `avy-all-windows' (ARG negates it)."
+  (interactive (list current-prefix-arg))
+  (avy-with avy-goto-char
+    (avy-jump
+     
+     (regexp-opt xah-left-brackets)
+     :window-flip arg)))
 
-              (defhydra hydra-bracket-mov (:color red :hint nil)
-                "
+(defhydra hydra-bracket-mov (:color red :hint nil :pre (hs-minor-mode))
+  "
               ^By List^             ^By Level^           ^Actions^
               ^^^^^^^^----------------------------------------------
-              _i_: next          _e_: higher
+              _i_: next          _e_: higher        _t_: toggle-hs
               _h_: prev          _n_: inner         _m_: mark-sexp
               _f_: end-of        _w_: avy-word
               "
-                ("q" nil)
-                (";" nil)
-                ("i" my/smart-forward-list)
-                ("h" backward-list)
-                ("e" backward-up-list)
-                ("n" down-list)
-                ("f" forward-list)
-                ("w" avy-goto-word-1 :exit t)
-                ("a" avy-goto-open-brackets)
-                ("m" mark-sexp))
+  ("q" nil)
+  (";" nil)
+  ("i" my/smart-forward-list)
+  ("h" backward-list)
+  ("e" backward-up-list)
+  ("n" down-list)
+  ("t" (progn
+         (hs-toggle-hiding)
+         (backward-up-list)))
+  ("s" consult-line)
+  ("f" forward-list)
+  ("w" avy-goto-word-1 :exit t)
+  ("a" avy-goto-open-brackets)
+  ("m" mark-sexp))
 
-(defhydra hydra-diff-hl (:color red)
-                "diff hunk"
-                ("q" nil)
-                ("n" diff-hl-next-hunk)
-                ("e" diff-hl-previous-hunk))
-
-              (defhydra hydra-move-by (:color red :exit t)
-                "move by"
-                ("n" hydra-vi/body "Char")
-                ("o" hydra-bracket-mov/body "Bracket")
-                ("h" hydra-diff-hl/body "HL diff")
-                
-                ("p f" project-find-file "Project FF" :exit nil)
-                ("b" hydra-buff-commands/body )
-                
-                )
-
-              (defhydra hydra-buff-commands (:color red :post (hydra-move-by/body))
-                ("b t" beginning-of-buffer "Buffer Top" :exit t)
-                ("b e" end-of-buffer "Buffer End" )
-                ("b s" switch-to-buffer "Buffer Switch" )
-                ;;("p f" project-find-file "Project FF" :exit nil)
-                )
-
-(defhydra hydra-vi ()
-  "vi"
-  ("h" backward-char)
-  ("n" next-line)
-  ("e" previous-line)
-  ("i" forward-char)
-  ("m" set-mark-command)
-("q" nil)
+(defhydra hydra-buff-commands (:color red :post (hydra-move-by/body))
+  ("b t" beginning-of-buffer "Buffer Top" :exit t)
+  ("b e" end-of-buffer "Buffer End" )
+  ("b s" switch-to-buffer "Buffer Switch" )
+  ;;("p f" project-find-file "Project FF" :exit nil)
   )
 
-(global-set-key (kbd "C-c m") #'hydra-move-by/body)
 
-(defhydra hydra-flycheck ()
-  "flycheck errors"
-  ("f" flycheck-next-error)
-  ("p" flycheck-previous-error)
-("q" nil)
-  )
+(defhydra hydra-avy ()
+  ("q" nil)
+  ("i" avy-goto-char "char")
+  ("I" consult-imenu "imenu")
+  ("n" avy-goto-line "line")
+  ("e" my/avy-goto-end-of-line "line end")
+  ("v" scroll-up-command "down")
+  ("u" scroll-down-command "up")
+  ("l" recenter-top-bottom "center")
+  ("s" consult-line "search")
+  ("m" set-mark-command "mark")
+  ("p" project-find-file "proj-file")
+  ("." consult-buffer)
+  ("b" consult-project-buffer "buffer")
+  
+  ("x" avy-goto-xah-open "open")
+  ("r" anzu-replace-at-cursor-thing "replace")
+  ("RET" back-to-avy/body :exit t))
+
+
+(defhydra back-to-avy (:color pink)
+("/" hydra-avy/body :exit t)
+("z" nil))
+
+(defhydra hydra-reading-helper (:color pink
+                                :base-map (make-sparse-keymap))
+  ("<escape>" nil)
+  ("SPC" (progn
+           (insert " ")
+           (keyboard-quit)) "insert space")
+  (";" scroll-other-window "scroll"))
